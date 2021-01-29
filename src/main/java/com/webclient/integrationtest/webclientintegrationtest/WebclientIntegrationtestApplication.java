d41f5bc6c46dd96e11f66416c72087960536e16a
Tim B <tbatty7@gmail.com>
2021-01-28 22:57:33 -0500

    initial commit

---
+++ src/main/java/com/webclient/integrationtest/webclientintegrationtest/WebclientIntegrationtestApplication.java
@@ -0,0 +1,13 @@
+package com.webclient.integrationtest.webclientintegrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientIntegrationtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientIntegrationtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webclient/integrationtest/webclientintegrationtest/WebclientIntegrationtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webclient.integrationtest.webclientintegrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientIntegrationtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
