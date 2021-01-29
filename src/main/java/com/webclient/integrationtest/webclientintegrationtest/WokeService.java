3d8aaa5ca89769490e040fec3c62553a469de309
Tim B <tbatty7@gmail.com>
2021-01-28 23:57:50 -0500

    moved classes to right place and added webclient to service

--- src/main/java/com/webclient/integrationtest/webclientintegrationtest/WokeService.java
+++ src/main/java/com/webclient/integrationtest/webclientintegrationtest/WokeService.java
@@ -1,9 +1,16 @@
 package com.webclient.integrationtest.webclientintegrationtest;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class WokeService {
+    private WebClient webClient;
+
+    public WokeService(WebClient wokeWebClient) {
+        this.webClient = wokeWebClient;
+    }
+
     public WokeResponse getAlarms() {
         return null;
     }
