dcbea0768a9e3b360e81788a8e418fb81aa5ef2e
Tim B <tbatty7@gmail.com>
2021-01-29 00:11:52 -0500

    added url property to WebClientConfig and got Lombok working

--- src/main/java/com/webclient/integrationtest/webclientintegrationtest/WebClientConfig.java
+++ src/main/java/com/webclient/integrationtest/webclientintegrationtest/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.webclient.integrationtest.webclientintegrationtest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    private String baseUrl;
+
+    public WebClientConfig(@Value("${base-url}") String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
     @Bean(name = "wokeWebClient")
     WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8880")
+                .baseUrl(baseUrl)
                 .build();
     }
 
--- src/main/java/com/webclient/integrationtest/webclientintegrationtest/WokeResponse.java
+++ src/main/java/com/webclient/integrationtest/webclientintegrationtest/WokeResponse.java
@@ -1,37 +1,16 @@
 package com.webclient.integrationtest.webclientintegrationtest;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class WokeResponse {
-    public WokeResponse(String alarm1, String alarm2, String alarm3) {
-        this.alarm1 = alarm1;
-        this.alarm2 = alarm2;
-        this.alarm3 = alarm3;
-    }
-
-    public String getAlarm1() {
-        return alarm1;
-    }
-
-    public String getAlarm2() {
-        return alarm2;
-    }
-
-    public String getAlarm3() {
-        return alarm3;
-    }
-
-    public void setAlarm1(String alarm1) {
-        this.alarm1 = alarm1;
-    }
-
-    public void setAlarm2(String alarm2) {
-        this.alarm2 = alarm2;
-    }
-
-    public void setAlarm3(String alarm3) {
-        this.alarm3 = alarm3;
-    }
-
     private String alarm1;
     private String alarm2;
     private String alarm3;
