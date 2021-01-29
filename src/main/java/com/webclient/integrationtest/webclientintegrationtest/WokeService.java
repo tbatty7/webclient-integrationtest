b842f45c25a012c63ad63ff573c9f64f4be8e820
Tim B <tbatty7@gmail.com>
2021-01-29 00:02:41 -0500

    implemented service call

--- src/main/java/com/webclient/integrationtest/webclientintegrationtest/WokeService.java
+++ src/main/java/com/webclient/integrationtest/webclientintegrationtest/WokeService.java
@@ -2,6 +2,9 @@ package com.webclient.integrationtest.webclientintegrationtest;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 public class WokeService {
@@ -12,6 +15,12 @@ public class WokeService {
     }
 
     public WokeResponse getAlarms() {
-        return null;
+        Mono<WokeResponse> wokeResponseMono = webClient.get()
+                .uri("/api/clock/alarms")
+                .header("Identification-Id", "1234")
+                .retrieve()
+                .bodyToMono(WokeResponse.class);
+
+        return wokeResponseMono.block(Duration.ofSeconds(30));
     }
 }
