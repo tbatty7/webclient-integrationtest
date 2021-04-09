331448370378136d40955f6f7fd7f707db3876a5
Tim B <tbatty7@gmail.com>
2021-04-09 17:53:50 -0400

    Added tests that include testing the request body on a POST call with WebClient

--- src/test/java/com/webclient/integrationtest/webclientintegrationtest/service/WokeServiceMockWebServerPostTest.java
+++ src/test/java/com/webclient/integrationtest/webclientintegrationtest/service/WokeServiceMockWebServerPostTest.java
@@ -5,14 +5,18 @@ import com.webclient.integrationtest.webclientintegrationtest.WokeResponse;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class WokeServiceMockWebServerPostTest {
@@ -39,10 +43,52 @@ class WokeServiceMockWebServerPostTest {
     }
 
     @Test
-    void returnsResponseFromExternalService() {
+    void returnsResponseFromExternalService() throws InterruptedException {
         mockExternalEndpoint(200, "{\"alarm1\": \"Hello World\"}");
+
         WokeResponse response = wokeService.addAlarm(buildRequest());
+
         Assertions.assertThat(response).isEqualTo(WokeResponse.builder().alarm1("Hello World").build());
+        mockServer.takeRequest();
+    }
+
+    @Test
+    void urlPathIsCorrect() throws InterruptedException {
+        mockExternalEndpoint(200, "{\"alarm1\": \"Hello World\"}");
+
+        wokeService.addAlarm(buildRequest());
+
+        RecordedRequest recordedRequest = mockServer.takeRequest();
+        Assertions.assertThat(recordedRequest.getPath()).isEqualTo("/api/clock/alarms");
+    }
+
+    @Test
+    void headerIsCorrect() throws InterruptedException {
+        mockExternalEndpoint(200, "{\"alarm1\": \"Hello World\"}");
+
+        wokeService.addAlarm(buildRequest());
+
+        RecordedRequest recordedRequest = mockServer.takeRequest();
+        Assertions.assertThat(recordedRequest.getHeader("Identification-Id")).isEqualTo("1234");
+    }
+
+    @Test
+    void exceptionIsThrownOn500() throws InterruptedException {
+        mockExternalEndpoint(500, "{\"error\": \"Oh No!!\"}");
+
+        assertThrows(WebClientResponseException.class, () -> wokeService.addAlarm(buildRequest()));
+        mockServer.takeRequest();
+    }
+
+    @Test
+    void requestBodyIsCorrect() throws InterruptedException {
+        mockExternalEndpoint(200, "{\"alarm1\": \"Hello World\"}");
+
+        wokeService.addAlarm(buildRequest());
+
+        RecordedRequest recordedRequest = mockServer.takeRequest();
+        String expectedRequestBody = "[text={\"year\":0,\"month\":0,\"day\":0,\"hour\":0,\"message\":\"Hello World\"}]";
+        Assertions.assertThat(recordedRequest.getBody().readByteString().toString()).isEqualTo(expectedRequestBody);
     }
 
     private AlarmRequest buildRequest() {
